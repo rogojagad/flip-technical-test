77010485fed34424c574091177a4dc126c5642b9
rogojagad <rogojagadalit@gmail.com>
2021-03-18 16:20:28 +0700

    setup disbursement entity

---
+++ src/const/Disbursement.js
@@ -0,0 +1,13 @@
+const COLLECTION = "disbursement";
+
+const ATTRIBUTE_RECEIPT_URL = "receipt_url";
+const ATTRIBUTE_STATUS = "status";
+const ATTRIBUTE_TIME_SERVED = "time_served";
+const ATTRIBUTE_USER_ID = "user_id";
+
+export default Object.freeze({
+  ATTRIBUTE_RECEIPT_URL,
+  ATTRIBUTE_STATUS,
+  ATTRIBUTE_TIME_SERVED,
+  ATTRIBUTE_USER_ID,
+});
