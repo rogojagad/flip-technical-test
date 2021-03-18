2ce90cf5e95023c3961af0cc0f049f3fca0f5631
rogojagad <rogojagadalit@gmail.com>
2021-03-18 15:14:42 +0700

    init seeder

---
+++ src/seed/Seeder.js
@@ -0,0 +1,4 @@
+import User from "@src/const/User";
+import Balance from "@src/const/Balance";
+import BalanceRepository from "@src/repository/BalanceRepository";
+import UserRepository from "@src/repository/UserRepository";
