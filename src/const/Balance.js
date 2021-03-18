ac8152f060563595db40a80de65ce5fbc2ad5f51
rogojagad <rogojagadalit@gmail.com>
2021-03-18 15:13:21 +0700

    add attribute const

---
+++ src/const/Balance.js
@@ -0,0 +1,10 @@
+const COLLECTION = "user";
+
+const ATTRIBUTE_AMOUNT = "amount";
+const ATTRIBUTE_USER_ID = "user_id";
+
+export default Object.freeze({
+  ATTRIBUTE_AMOUNT,
+  ATTRIBUTE_USER_ID,
+  COLLECTION,
+});
---
+++ src/const/User.js
@@ -0,0 +1,10 @@
+const COLLECTION = "user";
+
+const ATTRIBUTE_ID = "id";
+const ATTRIBUTE_NAME = "name";
+
+export default Object.freeze({
+  ATTRIBUTE_ID,
+  ATTRIBUTE_NAME,
+  COLLECTION,
+});
