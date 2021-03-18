fe69c06ff5ad03b0a38ee4e5683e7c5d3c810680
rogojagad <rogojagadalit@gmail.com>
2021-03-18 17:25:29 +0700

    add transaction id attribute

--- src/const/Disbursement.js
+++ src/const/Disbursement.js
@@ -4,10 +4,12 @@ const ATTRIBUTE_RECEIPT_URL = "receipt_url";
 const ATTRIBUTE_STATUS = "status";
 const ATTRIBUTE_TIME_SERVED = "time_served";
 const ATTRIBUTE_USER_ID = "user_id";
+const ATTRIBUTE_TRANSACTION_ID = "transaction_id";
 
 export default Object.freeze({
   ATTRIBUTE_RECEIPT_URL,
   ATTRIBUTE_STATUS,
   ATTRIBUTE_TIME_SERVED,
+  ATTRIBUTE_TRANSACTION_ID,
   ATTRIBUTE_USER_ID,
 });
