43f6582b87c30460666cb810e94774d8509d2fa9
rogojagad <rogojagadalit@gmail.com>
2021-03-18 23:33:43 +0700

    add disbursement http service

--- src/service/DisbursementHttpService.js
+++ src/service/DisbursementHttpService.js
@@ -1,1 +1,28 @@
-import {} from "";
+import Balance from "~src/const/Balance";
+import DisbursementHttpRepository from "~src/repository/DisbursementHttpRepository";
+import User from "~src/const/User";
+import DisbursementPayload from "~src/const/DisbursementPayload";
+
+export default class DisbursementHttpService {
+  constructor() {
+    this.disbursementHttpRepository = new DisbursementHttpRepository();
+  }
+
+  async sendOneDisbursement(user, balance, remark = "") {
+    const payload = Object();
+
+    payload[DisbursementPayload.ATTRIBUTE_ACCOUNT_NUMBER] =
+      user[User.ATTRIBUTE_ACCOUNT_NUMBER];
+    payload[DisbursementPayload.ATTRIBUTE_BANK_CODE] =
+      user[User.ATTRIBUTE_BANK_CODE];
+    payload[DisbursementPayload.ATTRIBUTE_AMOUNT] =
+      balance[Balance.ATTRIBUTE_AMOUNT];
+    payload[DisbursementPayload.ATTRIBUTE_REMARK] = remark;
+
+    const result = await this.disbursementHttpRepository.sendDisbursement(
+      payload
+    );
+
+    return result;
+  }
+}
