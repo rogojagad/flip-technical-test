d1b3030c430febcaa18a4a4d298d16c78240275d
rogojagad <rogojagadalit@gmail.com>
2021-03-18 17:32:51 +0700

    add account number and bank code attr

--- src/const/User.js
+++ src/const/User.js
@@ -1,8 +1,12 @@
 const COLLECTION = "user";
 
+const ATTRIBUTE_ACCOUNT_NUMBER = "account_number";
+const ATTRIBUTE_BANK_CODE = "bank_code";
 const ATTRIBUTE_NAME = "name";
 
 export default Object.freeze({
+  ATTRIBUTE_ACCOUNT_NUMBER,
+  ATTRIBUTE_BANK_CODE,
   ATTRIBUTE_NAME,
   COLLECTION,
 });
