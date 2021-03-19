647eb82942159e2373eed518c2397e6e547f09f4
rogojagad <rogojagadalit@gmail.com>
2021-03-19 16:51:21 +0700

    add truncate before seeding

--- src/seed/Seeder.js
+++ src/seed/Seeder.js
@@ -1,14 +1,37 @@
+import { deleteQueryBatch } from "~src/seed/Truncator";
 import User from "~src/const/User";
 import Balance from "~src/const/Balance";
 import BalanceRepository from "~src/repository/BalanceRepository";
+import db from "~src/entity/Firebase";
+import Disbursement from "~src/const/Disbursement";
 import UserRepository from "~src/repository/UserRepository";
 
 const userRepository = new UserRepository();
 const balanceRepository = new BalanceRepository();
+const allCollections = [
+  User.COLLECTION,
+  Balance.COLLECTION,
+  Disbursement.COLLECTION,
+];
 
 seed();
 
+async function truncateCollection() {
+  const batchSize = 5;
+
+  allCollections.forEach((collection) => {
+    const collectionRef = db.collection(collection);
+    const query = collectionRef.orderBy("__name__").limit(batchSize);
+
+    return new Promise((resolve, reject) => {
+      deleteQueryBatch(db, query, resolve).catch(reject);
+    });
+  });
+}
+
 async function seed() {
+  console.log("Truncating Database");
+  await truncateCollection();
   const userIds = await seedUsers();
   await seedBalance(userIds);
   console.log("Done Seeding Database");
---
+++ src/seed/Truncator.js
@@ -0,0 +1,23 @@
+export async function deleteQueryBatch(db, query, resolve) {
+  const snapshot = await query.get();
+
+  const batchSize = snapshot.size;
+  if (batchSize === 0) {
+    // When there are no documents left, we are done
+    resolve();
+    return;
+  }
+
+  // Delete documents in a batch
+  const batch = db.batch();
+  snapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
+
+  // Recurse on the next process tick, to avoid
+  // exploding the stack.
+  process.nextTick(() => {
+    deleteQueryBatch(db, query, resolve);
+  });
+}
