67f28b7a6bab1c46711bf0b12547e0c4c387ba47
rogojagad <rogojagadalit@gmail.com>
2021-03-21 01:11:17 +0700

    revise read disbursement by user

--- src/controller/DisbursementController.js
+++ src/controller/DisbursementController.js
@@ -1,14 +1,20 @@
+import CreateDisbursementService from "~src/service/CreateDisbursementService";
 import ReadDisbursementService from "~src/service/ReadDisbursementService";
 import ResponseFactory from "~src/factory/ResponseFactory";
 
 export default class DisbursementController {
   constructor() {
+    this.createDisbursementService = new CreateDisbursementService();
     this.readDisbursementService = new ReadDisbursementService();
     this.responseFactory = new ResponseFactory();
   }
 
+  async createOneByUserId(userId, res) {
+    await
+  }
+
   async readManyByUserId(userId, res) {
-    const disbursements = this.readDisbursementService.readManyByUserId(userId);
+    const disbursements = await this.readDisbursementService.readManyByUserId(userId);
 
     return this.responseFactory.responseOk(res, disbursements);
   }
