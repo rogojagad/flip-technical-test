1610d6c0352b8fcc370b24c3729e85375ff642e0
rogojagad <rogojagadalit@gmail.com>
2021-03-21 01:38:56 +0700

    add more core attribute

--- src/const/BaseConst.js
+++ src/const/BaseConst.js
@@ -1,2 +1,9 @@
+const ATTRIBUTE_CREATED_AT = "created_at";
 const ATTRIBUTE_ID = "id";
-export default { ATTRIBUTE_ID };
+const ATTRIBUTE_UPDATED_AT = "updated_at";
+
+export default {
+  ATTRIBUTE_CREATED_AT,
+  ATTRIBUTE_ID,
+  ATTRIBUTE_UPDATED_AT,
+};
